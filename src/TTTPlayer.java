50a604953452a535ff13c6f304a10de1bc5abe1c
Alexander Wright <Alexwright45177@gmail.com>
2025-03-31 19:04:06 -0400

    Update TTTPlayer.java

--- src/TTTPlayer.java
+++ src/TTTPlayer.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
-
 public class TTTPlayer {
+    private String name;
+    private String symbol;
+
+    public TTTPlayer(String name, String symbol) {
+        this.name = name;
+        this.symbol = symbol;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
 }
