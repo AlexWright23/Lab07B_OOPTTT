17e09000c09f8469992e7bdfa3a4c79a35add4b0
Alexander Wright <Alexwright45177@gmail.com>
2025-03-31 19:02:51 -0400

    Update TTTBoard.java

--- src/TTTBoard.java
+++ src/TTTBoard.java
@@ -1,4 +1,38 @@
-package PACKAGE_NAME;
-
 public class TTTBoard {
+    private String[][] board;
+
+    public TTTBoard() {
+        board = new String[3][3];
+        resetBoard();
+    }
+
+    public void resetBoard() {
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++)
+                board[i][j] = "";
+    }
+
+    public boolean makeMove(int row, int col, String symbol) {
+        if (board[row][col].isEmpty()) {
+            board[row][col] = symbol;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean checkWin(String symbol) {
+        for (int i = 0; i < 3; i++) {
+            if (board[i][0].equals(symbol) && board[i][1].equals(symbol) && board[i][2].equals(symbol)) return true;
+            if (board[0][i].equals(symbol) && board[1][i].equals(symbol) && board[2][i].equals(symbol)) return true;
+        }
+        return board[0][0].equals(symbol) && board[1][1].equals(symbol) && board[2][2].equals(symbol) ||
+                board[0][2].equals(symbol) && board[1][1].equals(symbol) && board[2][0].equals(symbol);
+    }
+
+    public boolean isFull() {
+        for (String[] row : board)
+            for (String cell : row)
+                if (cell.isEmpty()) return false;
+        return true;
+    }
 }
