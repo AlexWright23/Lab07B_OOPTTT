0577c060940773d6aa87c8d911e60cb9a14d4719
Alexander Wright <Alexwright45177@gmail.com>
2025-04-02 16:20:29 -0400

    Update TTTTileButton.java

--- src/TTTTileButton.java
+++ src/TTTTileButton.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+import javax.swing.JButton;
 
-public class TTTTileButton {
+public class TTTTileButton extends JButton {
+    private int row;
+    private int col;
+
+    public TTTTileButton(int row, int col) {
+        this.row = row;
+        this.col = col;
+        setText("");
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getCol() {
+        return col;
+    }
 }
